34b1d5e5ddedde3ea67727419aa993840a62a69d
Roberto Torres <rm.torres010392@gmail.com>
2019-02-18 18:06:05 -0600

    about me added

---
+++ about/about_me.js
@@ -0,0 +1,2 @@
+particlesJS.load('particles-js', 'particles.json', function(){console.log('callback - particles.json config loaded');});
+$(window).on('load', function () { $(".load-fade").fadeOut("slow"); });
